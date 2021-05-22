d8d039dc3d6b4982894edb861a0c50fc2a0324fb
Kyle <kohlwiler@yahoo.com>
2021-05-21 18:02:57 -0700

    added FeedMe logo

--- src/components/Budget/Budget.js
+++ src/components/Budget/Budget.js
@@ -8,7 +8,7 @@ const Budget = () => {
       <img src={budgetPic} className="card-img-top" alt="BeeWell" id="beeWell" />
         <div className="card-body">
           <h3 className="card-title">Budget Tracker</h3>
-          <p className="card-text">Some classmates and I built a site to help you live a healthier lifestyle by looking up recipes that can fit the workout and diet you want to live by.</p>
+          <p className="card-text">This is an application I put together that is a budget tracker to help you manage a budget.</p>
         <a href="https://calm-chamber-87100.herokuapp.com/" className="btn btn-primary" target="_blank">Link to Project</a>
           <p></p>
         <a href="https://github.com/Ohskie3/Budget-Tracker" className="btn btn-primary" target="_blank">Link to GitHub</a>
--- src/components/Project3/Project3.js
+++ src/components/Project3/Project3.js
@@ -1,13 +1,13 @@
 import gramLogo from './reinstagram.png'
 
 
-const reinstagram = () => {
+const FeedMe = () => {
   return (
     <div className="card" style= {{ width: "18rem" }}>
-      <img src={gramLogo} className="card-img-top" alt="reinstagram" id="reinstagram" />
+      <img src="https://dewey.tailorbrands.com/production/brand_version_mockup_image/626/5308215626_d945acf3-4dcd-4b1f-bc88-d8f66e48d54e.png?cb=1621536314" className="card-img-top" alt="FeedMe" id="FeedMe" />
         <div className="card-body">
-          <h3 className="card-title">re-Instagram Project</h3>
-          <p className="card-text">Some classmates and I built a new social media site that is modeled after the poopular site Instagram.</p>
+          <h3 className="card-title">FeedMe Project</h3>
+          <p className="card-text">Some classmates and I built a new social media site that is modeled after the poopular site Instagram, that focusses on people making posts focussed on food.</p>
         <a href="https://mighty-mesa-72754.herokuapp.com/" className="btn btn-primary" target="_blank">Link to Project</a>
         <p></p>
         <a href="https://github.com/Ohskie3/reinstagram" className="btn btn-primary" target="_blank">Link to GitHub</a>
@@ -16,4 +16,4 @@ const reinstagram = () => {
   )
 }
 
-export default reinstagram
+export default FeedMe
